2333a7b66de27f612b14c4e31ade64d593ddf260
todocodeacademy <admin@todocodeacademy.com>
2026-04-14 16:39:57 -0300

    Se agrega clase 4

---
+++ Clase 4 - Objetos y Formularios (con IA)/src/main/java/com/example/todocodeacademy/ThymeleafProyecto/ThymeleafProyectoApplication.java
@@ -0,0 +1,14 @@
+package com.example.todocodeacademy.ThymeleafProyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafProyectoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ThymeleafProyectoApplication.class, args);
+	}
+
+}
