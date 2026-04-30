e6a703bfb70b6240aa7a2d43de23f5d3039ce143
todocodeacademy <admin@todocodeacademy.com>
2026-04-30 18:53:50 -0300

    Se agrega clase 5

---
+++ Clase 5 - Iteraciones en Thymeleaf/src/main/java/com/todocodeacademy/IteracionesThymeleaf/IteracionesThymeleafApplication.java
@@ -0,0 +1,14 @@
+package com.todocodeacademy.IteracionesThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IteracionesThymeleafApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(IteracionesThymeleafApplication.class, args);
+	}
+
+}
---
+++ Clase 5 - Iteraciones en Thymeleaf/src/test/java/com/todocodeacademy/IteracionesThymeleaf/IteracionesThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todocodeacademy.IteracionesThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IteracionesThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
