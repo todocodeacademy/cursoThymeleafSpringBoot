72c9c3494e2da472adad57dfdec6df81e1c4344f
todocodeacademy <admin@todocodeacademy.com>
2026-05-05 14:52:34 -0300

    Se agrega clase 6

---
+++ Clase 6 - Fragmentos en Thymeleaf/src/main/java/com/todocodeacademy/FragmentosThymeleaf/FragmentosThymeleafApplication.java
@@ -0,0 +1,14 @@
+package com.todocodeacademy.FragmentosThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragmentosThymeleafApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FragmentosThymeleafApplication.class, args);
+	}
+
+}
---
+++ Clase 6 - Fragmentos en Thymeleaf/src/test/java/com/todocodeacademy/FragmentosThymeleaf/IteracionesThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todocodeacademy.FragmentosThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IteracionesThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
