35dce491fc4cb52f0bd2e61e7a22256a8e2fcec7
todocodeacademy <admin@todocodeacademy.com>
2026-05-14 11:20:06 -0300

    Se agrega clase 7

---
+++ Clase 7 - Sesiones (HttpSessions) en Thymeleaf/src/main/java/com/todocodeacademy/sesiones/SesionesApplication.java
@@ -0,0 +1,13 @@
+package com.todocodeacademy.sesiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesionesApplication.class, args);
+	}
+
+}
---
+++ Clase 7 - Sesiones (HttpSessions) en Thymeleaf/src/test/java/com/todocodeacademy/sesiones/SesionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todocodeacademy.sesiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
